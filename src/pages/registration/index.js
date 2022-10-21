ec48e84b18a549cb0bd66a0bc4b27b88ac1e34fe
unknown <you@example.com>
2022-10-21 16:35:42 -0300

    firebase conectado no firebase

--- src/pages/registration/index.js
+++ src/pages/registration/index.js
@@ -1,52 +1,89 @@
 // Imports Geral
-import React from "react";
-import { View,Text,StyleSheet, TextInput } from "react-native";
+import React, {useState} from "react";
+import { View,Text,StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import firebase from '../firebaseConnection'
 
 export default function Registration() {
     
-    
+    const [nome,setNome] = useState("");
+    const [nota1,setNota1] = useState("");
+    const [nota2,setNota2] = useState("");
+    const [nota3,setNota3] = useState("");
+    const [imagem,setImagem] = useState("");
+
+    async function Cadastrar (){
+        
+        const Alunos = await firebase.database().ref('aluno');
+        const chave = Alunos.push().key;
+
+        Alunos.child(chave).set({
+
+            nome: nome,
+            nota1: nota1,
+            nota2: nota2,
+            nota3: nota3,
+            imagem: imagem
+
+        })
+
+        setNome("")
+        setNota1("")
+        setNota2("")
+        setNota3("")
+        setImagem("")
+    }
+
     return (
-        <View>
+
+        <View style={styles.cadastro}>
             <Text style={styles.home}>Screen Registration !</Text>
 
             <Text style={styles.home}>Nome: </Text>
-            <TextInput style={styles.inputes} placeholder="Insira seu Nome"></TextInput>
+            <TextInput value={nome} style={styles.inputes} placeholder="Insira seu Nome" onChangeText={(texto)=> setNome(texto)} ></TextInput>
             
             <Text style={styles.home}>Nota 1: </Text>
-            <TextInput style={styles.inputes} placeholder="Insira sua Nota 1"></TextInput>
+            <TextInput value={nota1} style={styles.inputes} placeholder="Insira sua Nota 1" onChangeText={(texto)=> setNota1(texto)} ></TextInput>
 
             <Text style={styles.home}>Nota 2: </Text>
-            <TextInput style={styles.inputes} placeholder="Insira sua Nota 2"></TextInput>
+            <TextInput value={nota2} style={styles.inputes} placeholder="Insira sua Nota 2" onChangeText={(texto)=> setNota2(texto)} ></TextInput>
 
             <Text style={styles.home}>Nota 3: </Text>
-            <TextInput style={styles.inputes} placeholder="Insira sua Nota 3"></TextInput>
+            <TextInput value={nota3} style={styles.inputes} placeholder="Insira sua Nota 3" onChangeText={(texto)=> setNota3(texto)} ></TextInput>
                 
             <Text style={styles.home}>Imagem: </Text>
-            <TextInput style={styles.inputes} placeholder="Insira sua Imagem"></TextInput>
+            <TextInput value={imagem} style={styles.inputes} placeholder="Insira sua Imagem" onChangeText={(texto)=> setImagem(texto)} ></TextInput>
+
+            <TouchableOpacity>
+                <Text style={styles.botao} onPress={Cadastrar}>Cadastrar</Text></TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    cadastro: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
     home: {
         textAlign: "center",
         paddingTop: "20px",
         textTransform: "capitalize",
         textDecorationLine: "underline"
     },
-
+    
     inputes: {
         borderColor: "#A9A9A9",
-        borderWidth: "5px", borderStyle: "ridge",
+        borderWidth: "5px",
+        borderStyle: "ridge",
         width: "350px",
-        /* 
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "top",
-        flexDirection: "column",
-        width: "100vw",
-        height: "100vh",
-        padding: "10px 10px 10px 10px",
-        */
+    },
+
+    botao: {
+        borderColor: "#A9A9A9",
+        borderWidth: "5px",
+        borderStyle: "ridge",
+        marginTop: 15
     }
 })
